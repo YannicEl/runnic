86533131afcf7f71b2808bbb7e2bdd81f995c4fd
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2022-06-14 21:20:32 +0200

    inital vue setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script setup lang="ts">
+if (!navigator.geolocation) {
+	console.log('geolocation is not supproted');
+} else {
+	navigator.geolocation.watchPosition(
+		(success) => {
+      success.coords
+    },
+		(error) => {}
+	);
+}
+</script>
+
+<style></style>
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import UnoCSS from 'unocss/vite';
+import { presetUno } from 'unocss';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		Vue({
+			reactivityTransform: true,
+		}),
+		Components(),
+		AutoImport({
+			imports: ['vue', 'vue/macros', '@vueuse/core'],
+			dirs: ['./src/composables'],
+			vueTemplate: true,
+		}),
+		UnoCSS({
+			shortcuts: {},
+			presets: [presetUno()],
+		}),
+	],
+});
